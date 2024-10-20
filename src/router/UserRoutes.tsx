f64845785ae80bcda8bc535b5c220192cab7259d
javier <nhat1234559@gmail.com>
2024-10-20 16:21:01 +0700

    fix git capitailize rollback

--- src/router/UserRoutes.tsx
+++ src/router/UserRoutes.tsx
@@ -2,25 +2,25 @@ import { lazy, Suspense } from 'react'
 
 import AppLoading from '~/components/common/AppLoading'
 import { PrivateLayout, PublicLayout } from '~/components/layout'
-import Register from '~/fix/pages/(auth)/register'
+import Register from '~/pages/(auth)/register'
 import AuthWrapper from '~/router/components/AuthRedirect'
 import PrivateRoute from '~/router/components/PrivateRoute'
 import { RouteNames } from '~/router/route-name'
 
 // Lazy load your components
-const Landing = lazy(() => import('~/fix/pages'))
-const App = lazy(() => import('~/fix/pages/app'))
-const AuthView = lazy(() => import('~/fix/pages/(auth)/Auth'))
-const Login = lazy(() => import('~/fix/pages/(auth)/login'))
-const DevView = lazy(() => import('~/fix/pages/dev-view'))
-const NotFoundView = lazy(() => import('~/fix/pages/not-found'))
-const Vocabulary = lazy(() => import('~/fix/pages/vocabulary'))
-const LearningSpace = lazy(() => import('~/fix/pages/space'))
-const Quiz = lazy(() => import('~/fix/pages/quiz'))
-const Essay = lazy(() => import('~/fix/pages/essay'))
-const EssayCreate = lazy(() => import('~/fix/pages/essay/[id]/essay-create'))
-const Setting = lazy(() => import('~/fix/pages/setting'))
-const EssayDetail = lazy(() => import('~/fix/pages/essay/[id]/essay-detail'))
+const Landing = lazy(() => import('~/pages'))
+const App = lazy(() => import('~/pages/app'))
+const AuthView = lazy(() => import('~/pages/(auth)/Auth'))
+const Login = lazy(() => import('~/pages/(auth)/login'))
+const DevView = lazy(() => import('~/pages/dev-view'))
+const NotFoundView = lazy(() => import('~/pages/not-found'))
+const Vocabulary = lazy(() => import('~/pages/vocabulary'))
+const LearningSpace = lazy(() => import('~/pages/space'))
+const Quiz = lazy(() => import('~/pages/quiz'))
+const Essay = lazy(() => import('~/pages/essay'))
+const EssayCreate = lazy(() => import('~/pages/essay/[id]/essay-create'))
+const Setting = lazy(() => import('~/pages/setting'))
+const EssayDetail = lazy(() => import('~/pages/essay/[id]/essay-detail'))
 
 const USER_ROUTES = [
   {
